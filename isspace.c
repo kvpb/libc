340aabadeed64d4a5c49c72b2088ba1a40844051
Karl <local-part@domain>
2021-10-20 16:35:42 +0200

    isalpha.c, isupper.c, islower.c, isspace.c and isdigit.c updated.

--- isspace.c
+++ isspace.c
@@ -1,41 +1,56 @@
-/*
-**
-** QWASAR.IO -- my_isspace
-**
-** @param {char} param_1
-**
-** @return {int}
-**
-*/
+/*// Copyright 2021 Karl V. P. Bertin
+////
+//// Redistribution and use in source and binary forms, with or without modifi-
+//// cation, are permitted provided that the following conditions are met:
+////
+////   1.  Redistributions of source code must retain the above copyright noti-
+////       ce, this list of conditions and the following disclaimer.
+////
+////   2.  Redistributions in binary form must reproduce the above copyright
+////       notice, this list of conditions and the following disclaimer in the
+////       documentation and/or other materials provided with the distribution.
+////
+////   3.  Neither the name of the copyright holder nor the names of its con-
+////       tributors may be used to endorse or promote products derived from
+////       this software without specific prior written permission.
+////
+//// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRI-
+//// BUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLA-
+//// RY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+//// OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSI-
+//// NESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+//// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHER-
+//// WISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+*/// OF THE POSSIBILITY OF SUCH DAMAGE.
 
 //#include <stdio.h> //	debug code
 
 int isspace(char c)
 {
-	return ( ( 9 <= c && c <= 13 || c == 32 ) ? 1 : 0 );
+	return ( ( ( 9 <= c && c <= 13 ) || ( c == 32 ) ) ? 1 : 0 );
 }
 
 /*int main()
 {
-	char c_0 = 'A';
-	char c_1 = 'a';
-	char c_2 = '0';
-	char c_3 = '\t';
-	char c_4 = '\n';
-	char c_5 = '\v';
-	char c_6 = '\f';
-	char c_7 = '\r';
-	char c_8 = ' ';
+	char c_0 = '\0';
+	char c_9 = '\t';
+	char c_10 = '\n';
+	char c_11 = '\v';
+	char c_12 = '\f';
+	char c_13 = '\r';
+	char c_32 = ' ';
+	char c_48 = '0';
 
-	printf("%d\n", isspace(c_0));
-	printf("%d\n", isspace(c_1));
-	printf("%d\n", isspace(c_2));
-	printf("%d\n", isspace(c_3));
-	printf("%d\n", isspace(c_4));
-	printf("%d\n", isspace(c_5));
-	printf("%d\n", isspace(c_6));
-	printf("%d\n", isspace(c_7));
-	printf("%d\n", isspace(c_8));
+	printf("NUL\t%d\n", isspace(c_0));
+	printf("HT\t%d\n", isspace(c_9));
+	printf("NL\t%d\n", isspace(c_10));
+	printf("VT\t%d\n", isspace(c_11));
+	printf("NP\t%d\n", isspace(c_12));
+	printf("CR\t%d\n", isspace(c_13));
+	printf("SP\t%d\n", isspace(c_32));
+	printf("0\t%d\n", isspace(c_48));
 	return 0;
-}*/
-//	debug code
+}*/ //	debug code
