22753cbdc5fc27738d36b5f36811be7ee526426e
Karl <local-part@domain>
2021-12-01 16:00:00 +0100

    fib.c updated.

--- fib.c
+++ fib.c
@@ -1,14 +1,32 @@
-/*
-**
-** QWASAR.IO -- my_fibonacci
-**
-**
-** @param {Integer} param_1
-** @return {integer}
+/*// Copyright 2021 Karl V. P. Bertin
+////
+//// Redistribution and use in source and binary forms, with or without modifi-
+//// cation, are permitted provided that the following conditions are met:
+////
+////   1.  Redistributions of source code must retain the above copyright noti-
+////       ce, this list of conditions and the following disclaimer.
+////
+////   2.  Redistributions in binary form must reproduce the above copyright
+////       notice, this list of conditions and the following disclaimer in the
+////       documentation and/or other materials provided with the distribution.
+////
+////   3.  Neither the name of the copyright holder nor the names of its con-
+////       tributors may be used to endorse or promote products derived from
+////       this software without specific prior written permission.
+////
+//// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRI-
+//// BUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLA-
+//// RY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+//// OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSI-
+//// NESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+//// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHER-
+//// WISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+*/// OF THE POSSIBILITY OF SUCH DAMAGE.
 
-**
-*/
-
+#include <math.h>
 //#include <stdio.h> //	debug code
 
 /*int pow( int b, int n )
@@ -25,23 +43,18 @@
 	else
 		return b * pow( b, n - 1 );
 }*/ // KVPB's RCSVPOW
+//	debug code
 
 /*int nfib( int n )
 {
-	if ( n >= 0 )
-		return fib( n );
-	if ( n == -1 ) //else if ( n == -1 )
-		return -( n );
-	else// if ( -2 >= n )
-		return pow( -1, n + 1 ) * ( nfib( n - 1 ) + nfib( n - 2 ) ); //pow( -1, n + 1 ) * fib( n );
+	return pow( -1, -( n ) + 1 ) * fib( -( n ) ); //pow( -1, n + 1 ) * fib( n );
 }*/ // KVPB's RCSVNFIB
+//	debug code
 
 int fib( int n )
 {
 	if ( n < 0 )
-		return -1;
-/*	if ( n < 0 )
-		return nfib( n );*/
+		return pow( -1, -( n ) + 1 ) * fib( -( n ) ); //return nfib( n );
 	if ( 0 <= n && n <= 1 ) //else if ( 0 <= n && n <= 1 ) // Else if? Why? It was a one-way trip to a return.
 		return n;
 	else
@@ -50,33 +63,47 @@ int fib( int n )
 
 /*int main()
 {
-	int   d = -20;
-	int   c =  -3;
-	int   b =  -2;
-	int   a =  -1;
-	int n_0 =   0;
-	int n_1 =   1;
-	int n_2 =   2;
-	int n_3 =   3;
-	int n_4 =  20;
+	//int d   = -20;
+	//int c   =  -3;
+	//int b   =  -2;
+	//int a   =  -1;
+	//int n_0 =   0;
+	//int n_1 =   1;
+	//int n_2 =   2;
+	//int n_3 =   3;
+	//int n_4 =  20;
+	int i =  0;
+	int j = -0;
 
-	printf("%d\n", fib( d   ));
-	printf("%d\n", fib( c   ));
-	printf("%d\n", fib( b   ));
-	printf("%d\n", fib( a   ));
-	printf("%d\n", fib( n_0 ));
-	printf("%d\n", fib( n_1 ));
-	printf("%d\n", fib( n_2 ));
-	printf("%d\n", fib( n_3 ));
-	printf("%d\n", fib( n_4 ));
+	//printf("%d\n", fib( d   ));
+	//printf("%d\n", fib( c   ));
+	//printf("%d\n", fib( b   ));
+	//printf("%d\n", fib( a   ));
+	//printf("%d\n", fib( n_0 ));
+	//printf("%d\n", fib( n_1 ));
+	//printf("%d\n", fib( n_2 ));
+	//printf("%d\n", fib( n_3 ));
+	//printf("%d\n", fib( n_4 ));
+	i = -20;
+	while ( -20 <= i && i <= 20 )
+	{
+		printf("F_%d\t= %d\n", i, fib( i ));
+		i++;
+	}
+	printf("F_-0\t= %d\n", fib( j ));
 	return 0;
-}*/ //	debug code
+}*/
+//	debug code
 
-// fib.c
-// Fibonacci number
-//
-// Karl V. P. Bertin `kvpb`
-// +33 A BB BB BB BB
-// local-part@domain
-// https://www.linkedin.com/in/karlbertin
+/*//	fib.c
+		Fibonacci number
 
+		Karl V. P. Bertin `kvpb`
+		+1 (DDD) DDD-DDDD
+		+33 A BB BB BB BB
+		local-part@domain
+		https://www.linkedin.com/in/karlbertin
+		https://twitter.com/kvpb777
+		https://github.com/kvpb
+		https://www.instagram.com/add/karlbertin
+*///	https://vm.tiktok.com/ZSwAmcFh/
