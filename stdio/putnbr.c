87b0f85289a2206a3ecf1a45fc8f9338c6095c2f
Karl <local-part@domain>
2022-04-23 10:45:17 +0200

    [libc/stdio/] putnbr.c updated.

--- stdio/putnbr.c
+++ stdio/putnbr.c
@@ -10,16 +10,19 @@
 ////
 *///  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "../include/stdio.h"
+//#include "../include/stdio.h"
 #include <stddef.h> // size_t
 #include <string.h> // strlen //	unused
 #include <unistd.h> // write
-#include <stdio.h> // putc, putchar, puts, printf //	unused //	debug
+//#include <stdio.h> // putc, putchar, puts, printf //	unused //	debug
 
 #undef strlen
-#undef putchar
-//#undef putstr
+#undef putn
+#undef uputn
+#undef ullputn
 #undef putnbr
+#undef uputnbr
+#undef ullputnbr
 
 size_t strlen(const char* s)
 {
@@ -29,17 +32,59 @@ size_t strlen(const char* s)
 	return ( p - s );
 } // KVPB's STRLEN
 
-int putchar(int c)
+int putn( int n, char* b)
 {
-	return write(1, &c, 1);
-} // KVPB's PUTCHAR
+	int i = 0;
+	int l = strlen( b );
 
-/*int putstr(const char* s)
+	if (  n  <  0 )
+	{
+		n = -( n );
+		i += write(1, "-", 1);
+	}
+	if (  n  <  l )
+		i += write(1, &b[n], 1);
+	else
+	{
+		i += putn( n / l, b );
+		i += putn( n % l, b );
+	}
+	return i;
+} // KVPB's PUTN
+int uputn( unsigned int n, char* b)
 {
-	return write(1, s, strlen(s));
-}*/ // KVPB's PUTSTR
+	int i = 0;
+	unsigned int l = strlen( b );
+
+	if (  n  <  l )
+		i += write(1, &b[n], 1);
+	else
+	{
+		i += uputn( n / l, b );
+		i += uputn( n % l, b );
+	}
+	return i;
+} // KVPB's UPUTN
+int ullputn( unsigned long long n, char* b)
+{
+	int i = 0;
+	unsigned long long l = strlen( b );
+
+	if (  n  <  l )
+		i += write(1, &b[n], 1);
+	else
+	{
+		i += ullputn( n / l, b );
+		i += ullputn( n % l, b );
+	}
+	return i;
+} // KVPB's ULLPUTN
 
 int putnbr( int n )
+{
+	return putn( n, "0123456789");
+} // KVPB's PUTNBR
+/*int putnbr( int n )
 {
 	int i = 0;
 
@@ -56,7 +101,17 @@ int putnbr( int n )
 	if (  n  < 10 )
 		i += putchar( n % 10 + '0');
 	return i;
-} // KVPB's PUTNBR
+}*/ // KVPB's PUTNBR
+
+int uputnbr( int n )
+{
+	return uputn( n, "0123456789");
+} // KVPB's UPUTNBR
+
+int ullputnbr( int n )
+{
+	return ullputn( n, "0123456789");
+} // KVPB's ULLPUTNBR
 
 /*int main()
 {
@@ -66,7 +121,7 @@ int putnbr( int n )
 	int d = 10;
 	int m = 100;
 	int n = 1000;
-	int x = -2147483648;
+	int x = -2147483647; //-2147483648;
 	int y = 2147483647;
 
 	printf("\n%i\n", putnbr( a )); //putnbr( a ); putchar('\n');
